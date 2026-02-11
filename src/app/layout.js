71045d2afb35513d841ceafac2ea70ef0c1d42cb
VishalAravind <vishalaravind03@gmail.com>
2026-02-12 00:10:19 +0530

    fix fb sdk init

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+// app/layout.js
 import "./globals.css";
 import { Toaster } from "sonner";
 import Script from "next/script";
@@ -14,19 +15,25 @@ export default function RootLayout({ children }) {
         {children}
         <Toaster richColors position="top-right" />
 
-        {/* Facebook SDK */}
+        {/* Load Facebook SDK */}
         <Script
           src="https://connect.facebook.net/en_US/sdk.js"
           strategy="afterInteractive"
-          onLoad={() => {
-            window.FB.init({
-              appId: "2088401315249131", // your real App ID
-              autoLogAppEvents: true,
-              xfbml: true,
-              version: "v24.0",
-            });
-          }}
         />
+
+        {/* Initialize SDK */}
+        <Script id="fb-init" strategy="afterInteractive">
+          {`
+            window.fbAsyncInit = function () {
+              FB.init({
+                appId: "2088401315249131",
+                autoLogAppEvents: true,
+                xfbml: true,
+                version: "v24.0"
+              });
+            };
+          `}
+        </Script>
       </body>
     </html>
   );
